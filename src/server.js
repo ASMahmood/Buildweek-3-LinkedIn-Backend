067192d15830984015a05f24686e70a82dc31702
Evgeni67 <evgeni313@abv.bg>
2021-01-25 18:08:03 +0500

    POSTS CRUD

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const postsRoutes = require("./services/posts");
 
 const server = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 server.use(cors());
 server.use(express.json());
+server.use("/posts", postsRoutes);
 
 mongoose
-  .connect(process.env.MONGO_ATLAS, {
+  .connect("mongodb+srv://evgeni:test1234@cluster0.0meed.mongodb.net/linkedin?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
