9fc0f4f6a9591a71a8914790586eae7e6b2d45b5
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-25 15:04:55 +0000

    stuff

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const postsRoutes = require("./services/posts");
-
+const commentsRouter = require("./services/comments");
 const profileRouter = require("./services/profiles");
 
 const server = express();
@@ -11,14 +11,17 @@ const port = process.env.PORT || 3002;
 server.use(cors());
 server.use(express.json());
 server.use("/posts", postsRoutes);
-
+server.use("/comment", commentsRouter);
 server.use("/profile", profileRouter);
 
 mongoose
-  .connect("mongodb+srv://evgeni:test1234@cluster0.0meed.mongodb.net/linkedin?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://evgeni:test1234@cluster0.0meed.mongodb.net/linkedin?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(
     server.listen(port, () => {
       console.log("The server's power level is over ", port);
