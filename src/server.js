2499712e4af1f87c43e24704aeccf90b94909fca
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-25 10:05:14 +0000

    created POST single user

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const profileRouter = require("./services/profiles");
+
 const server = express();
 const port = process.env.PORT || 3001;
 
 server.use(cors());
 server.use(express.json());
+
+server.use("/profile", profileRouter);
 
 mongoose
   .connect(process.env.MONGO_ATLAS, {
--- src/services/profiles/index.js
+++ src/services/profiles/index.js
@@ -5,7 +5,14 @@ const profileRouter = express.Router();
 
 profileRouter.post("/", async (req, res, next) => {
   try {
-    const profileWithImage = { ...req.body };
+    const profileWithImage = {
+      ...req.body,
+      image:
+        "https://res.cloudinary.com/dhmw620tl/image/upload/v1611568491/benchmark3/default-profile.png",
+    };
+    const newProfile = new ProfileModel(profileWithImage);
+    const savedProfile = await newProfile.save();
+    res.status(201).send(savedProfile);
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
